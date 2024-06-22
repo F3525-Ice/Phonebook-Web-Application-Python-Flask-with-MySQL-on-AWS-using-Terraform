3b6e1a7f653e2f3a5590235987f03bca1def3665
Erol <seraguneri@gmail.com>
2024-06-22 21:12:23 +0300

    completed

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "github-user"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "phonebook-key"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us"
+}
